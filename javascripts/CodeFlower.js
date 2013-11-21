3e33af26247c68243bb9ad4cc110e368dac36319
manmeghw <manmeghw@ubuntu.(none)>
2013-11-21 15:24:51 +0530

    Temp modified display tag

--- javascripts/CodeFlower.js
+++ javascripts/CodeFlower.js
@@ -123,13 +123,13 @@ CodeFlower.prototype.click = function(d) {
 CodeFlower.prototype.mouseover = function(d) {
   this.text.attr('transform', 'translate(' + d.x + ',' + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ')')
     .text(d.name + ": " + d.size + " loc")
-    .style('display', null);
+    .style('display', 'block');
 };
 
 CodeFlower.prototype.mouseout = function(d) {
   this.text.attr('transform', 'translate(' + d.x + ',' + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ')')
     .text(d.name + ": " + d.size + " loc")
-    .style('display', null);
+    .style('display', 'block');
 };
 
 CodeFlower.prototype.tick = function() {
